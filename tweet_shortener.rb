633c1d943f2fd0176405baf890ad366908b46af7
Ruby Weiner <rubyweiner796@me.com>
2018-06-18 19:38:23 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,40 @@
-# Write your code here.
+def dictionary
+  words = {
+    "hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "For" => '4',
+    "four" => '4',
+    "be" => 'b',
+    "you" => 'u',
+    "at" => '@',
+    "and" => '&'
+  }
+end
+
+def word_substituter(tweet)
+  shortened_tweet = []
+  dictionary_a = dictionary.keys
+  tweet_a = tweet.split(' ')
+  tweet_a.each do |word|
+    dictionary_a.include?(word) ? shortened_tweet << dictionary[word] : shortened_tweet << word
+  end
+  shortened_tweet.join(' ')
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweets)
+  if tweets.length > 140
+    word_substituter(tweets)
+  end
+
+end
+  
+  
